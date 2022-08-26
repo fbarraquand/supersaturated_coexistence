390985caca1d920b6a2dff3fd32b754328b0e82f
gggggguillaume <107686092+DoyenGuillaume@users.noreply.github.com>
2022-08-27 00:19:36 +0200

    Changes to Fig 3

--- Code/Script_Fig_3.R
+++ Code/Script_Fig_3.R
@@ -22,15 +22,16 @@ library(patchwork)
   Fig_3b <- {Fig_3a + coord_cartesian(xlim = c(0.2, 0.4), ylim = c(0, 25))} 
   
   Fig_3c <- {ggplot(dataresults_3cd) + 
-      geom_point(aes(x = K, y = Minimum), color = "red") + 
-      geom_point(aes(x = K, y = Maximum), color = "blue") + 
+      geom_point(aes(x = K, y = Minimum), color = "blue") + 
+      geom_point(aes(x = K, y = Maximum), color = "red") + 
       theme_classic() + 
+      labs(x = "K", y = "Extremums") + 
       coord_cartesian(xlim = c(0.1, 0.5),ylim = c(0, 100))}
   
   Fig_3d <- {Fig_3c+coord_cartesian(xlim = c(0.2, 0.4),ylim = c(0, 25))}
   
-  Fig_3 <- {Fig_3a + Fig_3b + Fig_3c + Fig_3d} + plot_annotation(tag_levels = "a")
+  Fig_3 <- {Fig_3a + Fig_3b + Fig_3c + Fig_3d} + plot_annotation(tag_levels = "a", tag_suffix = ")")
   
-  ggsave("./Figures/Figure_3.png")
+  ggsave("./Figures/Figure_3.png", dpi = 500)
   
 }
